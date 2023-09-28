b767a8bb18250d160cad1bad48f562f88d7fa490
unknown <pietari.m@hotmail.com>
2023-09-29 01:15:33 +0300

    front end shows up for some reason backend not yet working

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const baseURL = process.env.REACT_APP_BACKEND_URL
+/* const baseURL = process.env.REACT_APP_API_URL */
+const baseURL = 'http://localhost:8080/4000/todos'
+console.log('alla on baseURL')
+console.log(baseURL)
 
 const apiClient = axios.create({
   baseURL: `${baseURL}`,
