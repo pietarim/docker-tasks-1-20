e5f9056ef86ac58f318ec832beae36a51aef2eda
unknown <pietari.m@hotmail.com>
2023-09-29 17:49:00 +0300

    all should work

--- todo-app/todo-backend/app.js
+++ todo-app/todo-backend/app.js
@@ -7,14 +7,19 @@ const todosRouter = require('./routes/todos')
 const statisticsRouter = require('./routes/statistics')
 
 const app = express()
+const yellMiddleware = (req, res, next) => {
+  console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+  next()
+}
 console.log('this even starts')
 app.use(cors())
+app.use(yellMiddleware)
 
 app.use(logger('dev'))
 app.use(express.json())
 
-app.use('/', indexRouter)
-app.use('/todos', todosRouter)
-app.use('/statistics', statisticsRouter)
+app.use('/api/', indexRouter)
+app.use('/api/todos', todosRouter)
+app.use('/api/statistics', statisticsRouter)
 
 module.exports = app
--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 /* const baseURL = process.env.REACT_APP_API_URL */
-const baseURL = 'http://localhost:8080/4000/todos'
+const baseURL = 'http://localhost:8080/api'
 console.log('alla on baseURL')
 console.log(baseURL)
 
